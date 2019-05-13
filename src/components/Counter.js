518b29e07c49e2cec50ba8d752fc207547f80643
erik-sandoval <erik.sandoval@icloud.com>
2019-05-13 14:22:10 -0700

    finished mvp

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -17,16 +17,18 @@ class Counter extends Component {
         // Fill in the two button onClick methods
         // Upon clicking these buttons, the count
         // should decrement or increment accordingly
+        console.log(this.props)
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={() => { this.props.increment() }}>
                     +
                 </button>
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={() => { this.props.decrement() }}>
                     -
                 </button>
-                 {/* Uncomment these button tags if you got
+
+                {/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
                 {/* <button onClick={this.incrementIfOdd}>
                     Increment if odd
